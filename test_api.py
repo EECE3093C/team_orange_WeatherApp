6aa0d412b2804913daf42fe73d464db517e24d1d
P0NYB0Y <will.a.schreiner@gmail.com>
2023-03-02 00:42:17 +0000

    formatting

--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
 import requests, json
+
 latitude = str(39.13)
 longitude = str(84.51)
 temp_unit = "fahrenheit"
@@ -6,5 +7,6 @@ windspeed_unit = "mph"
 precipitation_unit = "inch"
 timezone = "America%2FNew_York"
 weather_api_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=weathercode&current_weather=true&temperature_unit=" + temp_unit +"&windspeed_unit=" + windspeed_unit + "&precipitation_unit=" + precipitation_unit +"&timezone=" + timezone
+
 response = requests.get(weather_api_url)
 print(json.dumps(response.json(), indent=4, sort_keys=True))
