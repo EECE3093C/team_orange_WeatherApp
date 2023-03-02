daa3a9d99bf4f456e77da612ef6625978236f1cb
P0NYB0Y <will.a.schreiner@gmail.com>
2023-03-02 01:49:32 +0000

    added funct to get weathercode and pull def

--- test_api.py
+++ test_api.py
@@ -1,6 +1,7 @@
-import requests, json
+import json
+import requests
 
-latitude = str(39.13)
+latitude = str(39.10)
 longitude = str(84.51)
 temp_unit = "fahrenheit"
 windspeed_unit = "mph"
@@ -8,5 +9,8 @@ precipitation_unit = "inch"
 timezone = "America%2FNew_York"
 weather_api_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=weathercode&current_weather=true&temperature_unit=" + temp_unit +"&windspeed_unit=" + windspeed_unit + "&precipitation_unit=" + precipitation_unit +"&timezone=" + timezone
 
-response = requests.get(weather_api_url)
-print(json.dumps(response.json(), indent=4, sort_keys=True))
+response = requests.get(weather_api_url).json()
+with open('./WMO_Codes.json', 'r',encoding= 'UTF-8') as file:
+    wmo_codes = json.loads(file.read())
+
+print ("The weather is " + wmo_codes[str(response['current_weather']['weathercode'])])
