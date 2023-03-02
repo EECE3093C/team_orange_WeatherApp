d436c7064cc889a1d209ce9ebbeac5befa4bc903
P0NYB0Y <will.a.schreiner@gmail.com>
2023-03-02 00:41:54 +0000

    added input parameters to api string

--- test_api.py
+++ test_api.py
@@ -1,4 +1,10 @@
-import requests
-weather_api_url = "https://api.open-meteo.com/v1/forecast?latitude=39.13&longitude=-84.51&daily=weathercode&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York"
+import requests, json
+latitude = str(39.13)
+longitude = str(84.51)
+temp_unit = "fahrenheit"
+windspeed_unit = "mph"
+precipitation_unit = "inch"
+timezone = "America%2FNew_York"
+weather_api_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=weathercode&current_weather=true&temperature_unit=" + temp_unit +"&windspeed_unit=" + windspeed_unit + "&precipitation_unit=" + precipitation_unit +"&timezone=" + timezone
 response = requests.get(weather_api_url)
-print(response.json())
+print(json.dumps(response.json(), indent=4, sort_keys=True))
